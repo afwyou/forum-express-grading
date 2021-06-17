bae38a2d6cc00315ea51bdc361a170b37de03d2c
afwyou <afwyousenge@gmail.com>
2021-06-17 10:28:51 +0800

    Add navbar to show login status

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  console.log(res.locals)
   next()
 })
 
