ac1827d96c1425aad11927f670bd48d266158bf4
afwyou <afwyousenge@gmail.com>
2021-06-23 07:15:28 +0800

    Add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,21 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
+
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({ include: Category }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r.dataValues,//展開運算子 
+        description: r.dataValues.description.substring(0, 50),//截斷文字
+        categoryName: r.Category.name//直接給定 categoryName 屬性，把類別名稱放進來
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 module.exports = restController
