47058edd1b7ecdeed379057d3095763135dbbf58
afwyou <afwyousenge@gmail.com>
2021-06-26 17:02:24 +0800

    question about database model

--- models/user.js
+++ models/user.js
@@ -36,6 +36,10 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     isAdmin: DataTypes.BOOLEAN,
     image: DataTypes.STRING
+    //不是很了解為什麼，在測試及開發模式下，app跟本地資料庫的互動
+    //好像啟動localhost監聽時，app使用的依然是開發模式的資料庫
+    //在測試模式新增的model，因為開發模式沒有執行，回到開發模式執行migrate後，user的seed資料就不見了
+    //因此必須重執行一次種子資料
   }, {
     sequelize,
     modelName: 'User',
