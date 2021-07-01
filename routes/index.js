31f43261019bef5e4d993823e53c82777767f93c
afwyou <afwyousenge@gmail.com>
2021-07-01 10:30:30 +0800

    explain where passport goes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 let routes = require('./routes');
 let apis = require('./apis')
 
-module.exports = (app) => {
+module.exports = (app) => {//passport原本在這裡當參數，是因為這裡原本有使用到passport的路由認證
+  //app.js本身有passport的引用，又引用這裡輸出的module的route所關聯的關係
   app.use('/', routes);
   app.use('/api', apis)
 }
